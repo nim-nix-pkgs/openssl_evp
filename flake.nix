b3d800653768bfca67cf68111dd66b0e221b3ad6
hugosenari <hugosenari@gmail.com>
2022-03-12 02:21:25 -0300

    chore: re index openssl_evp

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Wrapper for OpenSSL's EVP interface'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."openssl_evp-master".url = "path:./master";
+  inputs."openssl_evp-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."openssl_evp-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
